1674f31304f55744aa35943f7c10c6f5f667ce52
hello <hello@ifnot.cc>
2017-05-15 18:02:26 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate rand;
+
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the Number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("the secret number is: {}", secret_number);
+
+    println!("Plz input your guess.");
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read Line");
+
+    println!("Your Guessed: {}", guess);
 }
