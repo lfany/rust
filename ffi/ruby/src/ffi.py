d9816fdfba47048528192a9c6f31e989feb1c44c
hello <hello@ifnot.cc>
2017-05-24 18:10:27 +0800

    update ffi

---
+++ ffi/ruby/src/ffi.py
@@ -0,0 +1,22 @@
+from ctypes import cdll
+
+import platform, os
+
+os_family = platform.system()
+
+print(os_family)
+print(os.getcwd())
+if os.getcwd().endswith('src'):
+    os.chdir('..')
+    print(os.getcwd())
+
+if os_family == 'Windows':
+    lib = cdll.LoadLibrary('target\\release\\ruby.dll')
+elif os_family == 'Linux':
+    lib = cdll.LoadLibrary('target/release/libruby.so')
+else:
+    lib = cdll.LoadLibrary('target/release/libruby.so')
+
+lib.process()
+
+print('done!')
